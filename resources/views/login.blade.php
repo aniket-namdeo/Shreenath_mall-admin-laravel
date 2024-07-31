f451e8e93c16ad183f521317704019863bbbeda7
himanshusm24 <himanshu.devgade@secondmedic.com>
2024-07-31 11:06:11 +0530

    login page update logo favicon added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,44 +1,76 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> Login </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-</head>
-<body>
-<div class="row justify-content-center mt-5">
-        <div class="col-lg-4">
-            <div class="card">
-                <div class="card-header">
-                    <h1 class="card-title">Login</h1>
-                </div>
-                <div class="card-body">
-                    @if(Session::has('error'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ Session::get('error') }}
+
+@include('backend.layouts.head')
+
+    <section class="login-page">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4 col-md-6 offset-md-3">
+                    @if(session()->has('error'))
+                        <div class="alert alert-danger">
+                            {{ session()->get('error') }}
                         </div>
                     @endif
-                    <form action="{{ route('login.user') }}" method="POST" autocomplete="off">
+                    <div class="logo-div mb-4 mb-xs-20">
+                        <img src="{{ asset('includes-backend/images/logo.png'); }}" alt="">
+                    </div>
+                    <form method="POST" action="{{ route('login.user') }}" autocomplete="off" >
                         @csrf
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email address</label>
-                            <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" class="form-control" id="password" required>
-                        </div>
-                        <div class="mb-3">
-                            <div class="d-grid">
-                                <button class="btn btn-primary">Login</button>
+                    <div class="card h-auto">
+                        <div class="upper-div d-flex align-items-center justify-content-center">
+                            <div class="text-center">
+                                <h6>Welcome Back Admin!</h6>
+                                <p>Sign in to continue to Shree nath mall</p>
                             </div>
                         </div>
+                        <div class="card-body">
+                            <div class="row mb-3">
+                                <label for="email">{{ __('Email Address') }}</label>
+    
+                                <div class="col-md-12">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+    
+                            <div class="row mb-3">
+                                <label for="password">{{ __('Password') }}</label>
+    
+                                <div class="col-md-12">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                {{-- <div class="form-check">
+                                    <input type="checkbox" id="check" class="form-check-input"> <label
+                                        class="form-check-label" for="check">Remember Me</label>
+                                </div> --}}
+                            </div>
+                            <button type="submit" class="btn web-btn w-100">
+                                Log In
+                            </button>
+                        </div>
+                    </div>
                     </form>
+                    <div class="mt-4 mt-xs-20 text-center">
+                        <div class="copyright-div">
+                            <p class="mb-0">{{ date('Y'); }} © SecondMedic, Designed with ❤️ by SecondMedic</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</body>
-</html>
+    </section>
+
+
+    @include('backend.layouts.js')
